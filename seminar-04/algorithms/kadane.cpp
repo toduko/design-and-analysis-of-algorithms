6e5024f20ae25c8658c74127e6e49d5da1ccab0a
toduko <teddyepich@gmail.com>
2024-03-15 12:33:42 +0200

    Fixed Kadane's algorithm code

--- seminar-04/algorithms/kadane.cpp
+++ seminar-04/algorithms/kadane.cpp
@@ -1,11 +1,11 @@
 int kadane(int *arr, int n)
 {
-    int max_so_far = -INF, max_ending_here = 0;
+    int max_so_far = arr[0], max_ending_here = arr[0];
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        max_so_far = max(arr[i], max_so_far + arr[i]);
-        max_ending_here = max(max_ending_here, max_so_far);
+        max_ending_here = max(max_ending_here + arr[i], arr[i]);
+        max_so_far = max(max_ending_here, max_so_far);
     }
 
     return max_so_far;
